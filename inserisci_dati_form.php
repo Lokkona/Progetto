54456ffccd50ec55b627e1b30e8eaeb46080d540
Marino Reppucci <marinoreppucci@air-di-marino.lan>
2025-02-23 00:58:52 +0100

    Gioggio

--- inserisci_dati_form.php
+++ inserisci_dati_form.php
@@ -70,7 +70,7 @@ $uname = $isLoggedIn ? $_SESSION["uname"] : '';
                 <h3>Tiri</h3>
                 <label for="tiri_t">Tentati</label>
                 <input type="number" id="tiri_t" name="tiri_t" min="0" required>
-                <label for="makes">Reaizzati</label>
+                <label for="makes">Realizzati</label>
                 <input type="number" name="makes" id="makes" min="0" required>
                 <h3>Tiri da 3</h3>
                 <label for="tiri3">Tentati</label>
