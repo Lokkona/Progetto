0cce936acf3fae4cea228fe6f52caef3af8a41eb
Target04 <obbi.vinci@gmail.com>
2025-02-22 15:03:33 +0100

    inserisci dati

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ $uname = $isLoggedIn ? $_SESSION['uname'] : '';
         <div class="row1">
             <a href="index.php">NOME SITO</a>
             <a href="inserisci_dati_form.php">AGGIUNGI PRESTAZIONE</a>
-            <a href="storico.html">STORICO</a>
+            <a href="storico.php">STORICO</a>
             <a href="statistiche_html.php">STATISTICHE</a>
         </div>
         <div class="row2">
